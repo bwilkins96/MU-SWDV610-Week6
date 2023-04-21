ef883ac7f21eed5332c2e9c6ad5a4e53e10e0cb3
bwilkins96 <ben.jr.speed@gmail.com>
2023-04-21 17:05:34 -0400

    search tree changes

--- b_search_tree.py
+++ b_search_tree.py
@@ -7,11 +7,16 @@ class BinarySearchTree:
     class _Node:
         """Node class containing a value and parent, left, and right node pointers"""
         
-        def __init__(self, val, parent=None, left=None, right=None):
+        def __init__(self, key, val, parent=None, left=None, right=None):
+            self._key = key
             self._val = val
             self._parent = parent
             self._left = left
             self._right = right
+        
+        def key(self):
+            """Returns the key variable of a node"""
+            return self._key
         
         def val(self):
             """Returns the value variable of a node"""
@@ -33,51 +38,60 @@ class BinarySearchTree:
             """Returns a list cotaining the left and right children nodes"""
             return [self._left, self._right]
 
-    def __init__(self, val=None):
+    def __init__(self, key=None, val=None):
         """Initializes a binary search tree based on the val parameter."""
         self._root = None
+        self._size = 0
 
-        if val:
-            if type(val) == list:
-                self.build_search_tree(val)
-            else: 
-                self._root = self._Node(val)
+        if key: 
+            if type(key) == list:
+                self.build_search_tree(key, val)
+            else:
+                self._root = self._Node(key, val)
     
     def get_root(self):
         """Returns the root node"""
         return self._root
 
-    def insert(self, val):
+    def put(self, key, val):
         """Inserts a node containing val into the search tree"""
         if self._root:
-            self._insert_into_tree(val)
+            self._insert_into_tree(key, val)
         else:
-            self._root = self._Node(val)
+            self._root = self._Node(key, val)
+        
+        self._size += 1
 
-    def _insert_into_tree(self, val):
+    def _insert_into_tree(self, key, val):
         """Nonpublic method that handles inserting a non-root node"""
-        new_node = self._Node(val)
+        new_node = self._Node(key, val)
         current = self._root
         inserted = False
 
         while not inserted:
-            if val < current._val:
+            if key < current._key:
                 if current._left:
                     current = current._left
                 else:
-                    current._left = self._Node(val, current)
+                    current._left = self._Node(key, val, current)
                     inserted = True
             else:
                 if current._right:
                     current = current._right
                 else:
-                    current._right = self._Node(val, current)
+                    current._right = self._Node(key, val, current)
                     inserted = True
 
-    def build_search_tree(self, lst):
-        """Inserts the values of lst into the search tree instance"""
-        for val in lst:
-            self.insert(val)
+    def build_search_tree(self, key_lst, val_lst=None):
+        """
+        Inserts the values of key_lst with corresponding values 
+        from val_list into the search tree instance
+        """
+        if val_lst == None:
+            val_lst = [None] * len(key_lst)
+
+        for i in range(len(key_lst)):
+            self.put(key_lst[i], val_lst[i])
 
     def preorder(self, node):
         """Generates a preorder iteration of nodes"""
@@ -102,12 +116,24 @@ class BinarySearchTree:
         for node in self.preorder(self._root):
             yield node
 
+    def __len__(self):
+        return self._size
+    
+    def __setitem__(self, key, val):
+        self.put(key, val)
+
 if __name__ == '__main__':
-    bst = BinarySearchTree(10)
-    bst.insert(5)
-    bst.insert(2)
-    bst.insert(3)
-    bst.insert(15)
+    bst = BinarySearchTree(10, 'a')
+    bst.put(5, 'b')
+    bst.put(2, 'c')
+    bst.put(15, 'e')
+    bst.put(3, 'd')
 
     for node in bst:
-        print(node._val)                  # -> 10, 5, 2, 3, 15
+        print(node.key(), node.val())                  # -> 10 a, 5 b, 2 c, 3 d, 15 e
+
+    print('\n----------------------\n')
+    bst2 = BinarySearchTree([10, 5, 2, 15, 3], ['a', 'b', 'c', 'e', 'd'])
+
+    for node in bst2:
+        print(node.key(), node.val())                  # -> 10 a, 5 b, 2 c, 3 d, 15 e
--- solutions.py
+++ solutions.py
@@ -7,8 +7,9 @@ from b_heap_tree import BinaryHeapTree
 def main():
     int_list = random_int_list()
 
+    b_heap_tree = BinaryHeapTree(int_list)
+
     b_search_tree = BinarySearchTree(int_list)
 
-    b_heap_tree = BinaryHeapTree(int_list)
 
 if __name__ == '__main__': main()
